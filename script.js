5e10890a4af1e2ac896dec01ad0bec5ae38f5f5e
Jefferey Einstein <monsterochieng@gmail.com>
2026-09-02 12:29:21 +0300

    Last chance

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const skills = ["Bash", "Linux", "Python",];
+const skills = ["Bash", "Linux", "Python"];
+
+let output = "";
+
 for (let i = 0; i < skills.length; i++) {
-console.log(skills[i]);
-} 
+    output += skills[i];
+
+    if (i < skills.length - 1) {
+        output += ", ";
+    }
+}
+
 if (skills.length > 2) {
-console.log("experienced");
+    output += " (experienced)";
 } else {
-console.log("beginner");
-} 
+    output += " (beginner)";
+}
+
 document.getElementById("output").textContent = output;
