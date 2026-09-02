c01841da088a4d63477778cbd3dcce60fa87d20a
Jefferey Einstein <monsterochieng@gmail.com>
2026-09-02 11:32:52 +0300

    Second commit: about me page

---
+++ script.js
@@ -0,0 +1,9 @@
+const skills = ["Bash", "Linux", "Python",];
+for (let i = 0; i < skills.length; i++) {
+console.log(skills[i]);
+} 
+if (skills.length > 2) {
+console.log("experienced");
+} else {
+console.log("beginner");
+} 
