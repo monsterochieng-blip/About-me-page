a603bb211206623bde0be52653998bb46f01073e
Jefferey Einstein <monsterochieng@gmail.com>
2026-09-04 16:30:34 +0300

    public_repos

--- script.js
+++ script.js
@@ -13,4 +13,12 @@ document.getElementById("output").textContent = result;
 const btn = document.querySelector("#toggleBtn");
 btn.addEventListener("click", function() {
 document.querySelector(".skills").classList.toggle("highlight"); });
+list = skills.join(", ")
+async function loadRepoCount() {
+  const response = await fetch("https://api.github.com/users/monsterochieng-blip");
+  const data = await response.json();
+  document.getElementById("repo-count").textContent = "Public repos: " + data.public_repos;
+}
+loadRepoCount();
+
 
